1bb5923c1e5cd758a6aaaebc0d6ff9fb65df46a0
n-akiyoshi <161280848+n-akiyoshi@users.noreply.github.com>
2024-04-15 11:29:46 +0900

    ファイルからデータを抜き出すを実装

--- app.js
+++ app.js
@@ -4,5 +4,13 @@ const readline = require('node:readline');
 const rs = fs.createReadStream('./popu-pref.csv');
 const rl = readline.createInterface({ input: rs});
 rl.on('line', lineString => {
-  console.log(lineString);
+  const columns = lineString.split(',');
+  const year = parseInt(columns[0]);
+  const prefecture = columns[1];
+  const popu = parseInt(columns[3]);
+  if (year === 2016 || year === 2021) {
+    console.log(year);
+    console.log(prefecture);
+    console.log(popu);
+  }
 });
